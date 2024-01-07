93e8a23b5e147ab2b65fa3ecb8748a5689eda435
akitkat <akitkat.cook.dx@gmail.com>
2024-01-07 21:00:38 +0900

    Update golf course JSON file name

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ mkdir -p static/data
 wget "${JSON_URL_HOTELS}" -O 'hotels.json'
 mv -v hotels.json static/data/
 
-# golfCourse.json
-wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourse.json'
-mv -v golfCourse.json static/data/
+# golfCourses.json
+wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourses.json'
+mv -v golfCourses.json static/data/
 
 node lib/contentful/fetch.js
 

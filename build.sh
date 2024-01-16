5235504d61456592b7102d5a666e6a5d0accbf79
akitkat <akitkat.cook.dx@gmail.com>
2024-01-16 21:15:39 +0900

    Update build script to remove unnecessary files and folders

--- build.sh
+++ build.sh
@@ -13,3 +13,17 @@ mv -v golfCourses.json static/data/
 node lib/contentful/fetch.js
 
 astro build
+
+rm -rf dist/amp
+cp -r dist amp
+mv amp dist
+cd dist/amp
+
+rm -rf categories tags elements images privacy-policy tags 404.html index.html
+
+# delete files exclude .html
+find ./ -type f ! -name "*.html" -delete
+
+# delete empty folders
+find ./ -type d -empty -delete
+

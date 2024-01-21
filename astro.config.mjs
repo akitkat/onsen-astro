9dc9b4534c8b5a17c4746add756c828c6cdd3d72
akitkat <akitkat.cook.dx@gmail.com>
2024-01-21 20:49:33 +0900

    Update Astro configuration file with integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,25 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  integrations: [react(), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), mdx(), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  })],
+  integrations: [
+    react(),
+    sitemap({
+      changefreq: "daily",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ],
   markdown: {
     remarkPlugins: [],
     shikiConfig: {

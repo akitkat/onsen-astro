01a0fc6ec33cc9b06a7040001e0d4970c2a3e4bb
akitkat <akitkat.cook.dx@gmail.com>
2024-01-22 20:59:47 +0900

    Add date_modified field to entry and config

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,7 @@ const postsCollection = defineCollection({
     categories: z.array(z.string()).default(["others"]),
     tags: z.array(z.string()).default(["others"]),
     draft: z.boolean().optional(),
+    date_modified: z.date().optional(),
   }),
 });
 

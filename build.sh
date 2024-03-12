7690d0f8a38cbc22cb0eb873fd42ec7aeb063ce2
akitkat <akitkat.cook.dx@gmail.com>
2024-03-12 21:28:24 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -3,30 +3,30 @@
 mkdir -p static/data
 
 # hotels.json
-wget "${JSON_URL_HOTELS}" -O 'hotels.json'
-mv -v hotels.json static/data/
+wget "${JSON_URL_HOTELS}" -O 'hotels.json' &&
+mv -v hotels.json static/data/ &&
 
 # golfCourses.json
-wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourses.json'
-mv -v golfCourses.json static/data/
+wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourses.json' &&
+mv -v golfCourses.json static/data/ &&
 
-node lib/contentful/fetch.js
+node lib/contentful/fetch.js &&
 
-astro build
+astro build &&
 
-rm -rf dist/amp
-cp -r dist amp
-mv amp dist
-cd dist/amp
+rm -rfv dist/amp &&
+cp -rv dist amp &&
+mv -v amp dist &&
+cd dist/amp &&
 
-rm -rf categories tags elements images privacy-policy tags 404.html index.html
+rm -rfv categories tags elements images privacy-policy tags 404.html index.html &&
 
 # delete files exclude .html
-find ./ -type f ! -name "*.html" -delete
+find ./ -type f ! -name "*.html" -delete &&
 
 # delete empty folders
-find ./ -type d -empty -delete
+find ./ -type d -empty -delete &&
 
-cd ../../
+cd ../../ &&
 
 node ampify.mjs

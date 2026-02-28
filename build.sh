40137980af29f182c0b11a83f7fe67d7006e54fd
akitkat <akitkat.cook.dx@gmail.com>
2026-02-28 12:14:01 +0900

    amp削除

--- build.sh
+++ build.sh
@@ -12,21 +12,4 @@ mv -v golfCourses.json static/data/ &&
 
 node lib/contentful/fetch.js &&
 
-astro build &&
-
-rm -rfv dist/amp &&
-cp -rv dist amp &&
-mv -v amp dist &&
-cd dist/amp &&
-
-rm -rfv categories tags elements images privacy-policy tags 404.html index.html &&
-
-# delete files exclude .html
-find ./ -type f ! -name "*.html" -delete &&
-
-# delete empty folders
-find ./ -type d -empty -delete &&
-
-cd ../../ &&
-
-node ampify.mjs
+astro build

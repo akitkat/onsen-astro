5803873d2878d56204d6d3459add2dc8105cf3b7
akitkat <akitkat.cook.dx@gmail.com>
2026-06-27 16:14:28 +0900

    refactor: remove golf course data handling and associated components

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ mkdir -p static/data
 wget "${JSON_URL_HOTELS}" -O 'hotels.json' &&
 mv -v hotels.json static/data/ &&
 
-# golfCourses.json
-wget "${JSON_URL_GOLF_COURSE}" -O 'golfCourses.json' &&
-mv -v golfCourses.json static/data/ &&
-
 node lib/contentful/fetch.js &&
 
 astro build
